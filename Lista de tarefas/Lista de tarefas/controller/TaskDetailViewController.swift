6435303a89809e895f3561cdf4771b8e494c6764
Gilberto Silva <betosilva.luz@gmail.com>
2021-02-06 19:29:54 -0300

    Finalizando layout do app.

--- Lista de tarefas/Lista de tarefas/controller/TaskDetailViewController.swift
+++ Lista de tarefas/Lista de tarefas/controller/TaskDetailViewController.swift
@@ -8,13 +8,8 @@
 import UIKit
 
 class TaskDetailViewController: UIViewController {
-    //var addButton: UIBarButtonItem = UIBarButtonItem(title: "test", style: .done, target: self, )
-    var addButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    
 }
-
--- Lista de tarefas/Lista de tarefas/controller/TaskTableTableViewController.swift
+++ Lista de tarefas/Lista de tarefas/controller/TaskTableTableViewController.swift
@@ -13,9 +13,6 @@ class TaskTableTableViewController: UITableViewController {
         super.viewDidLoad()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
