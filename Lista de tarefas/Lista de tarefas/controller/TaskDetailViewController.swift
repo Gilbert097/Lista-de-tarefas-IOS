1744acd152663e6ad0c632dba0f106009029443b
Gilberto Silva <betosilva.luz@gmail.com>
2021-02-05 23:09:52 -0300

    Criando as telas de lista de tarefas e criar uma nova tarefa.

---
+++ Lista de tarefas/Lista de tarefas/controller/TaskDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lista de tarefas
+//
+//  Created by Gilberto da Luz on 05/02/21.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+    //var addButton: UIBarButtonItem = UIBarButtonItem(title: "test", style: .done, target: self, )
+    var addButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
+
